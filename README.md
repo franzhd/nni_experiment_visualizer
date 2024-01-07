17f009b51eda18207d954fe14e00258f9ed8d6d1
franzhd <franzhd@live.it>
2024-01-07 17:38:59 +0100

    added function, try example and nni.sqlite of example

---
+++ README.md
@@ -0,0 +1,20 @@
+# How this work
+i discovered that nni save in the result folder, under the name of the experiment, stuff other than the trials themself.
+
+Among this stuff, i found a db.sqlite file that contains all the informations that nni shows in the web page.
+
+I tought that acceding that in the fist place can be hard at first but then can simplify a lot the experiment evaluation.
+
+Here there is a small description of the .sqlite and the procedure used for the data estraction.
+____
+## Explore the database
+![alt-text](images/db_content.png "DB content")
+
+As we can se from the above screenshot, there are three main tables: _ExperimentProfile, MetricData, TrialJobEvent_.
+
+- _ExperimentProfile_: Not used
+- _MetricData_: Contains all the results metrics of the runned trials of that experiment.
+- _TrialJobEvent_: Contains the paramenters chosen for each trials.
+
+___
+## input and output of the Function 
---
+++ src/NniDbQuery.py
@@ -0,0 +1,93 @@
+import sqlite3
+import json
+from tabulate import tabulate
+
+def merge_dicts(dict1, dict2)-> dict:
+    merged_dict = {}
+    for key in dict1.keys():
+        merged_dict[key] = [dict1[key], dict2[key]]
+    return merged_dict
+
+
+def nni_query(tiral_sqlite_path, show=True) -> dict:
+
+
+    table_name = 'MetricData'
+    # Connect to the SQLite database
+    connection = sqlite3.connect(tiral_sqlite_path)
+    cursor = connection.cursor()
+
+    query = f"""
+        SELECT trialjobId, sequence, data
+        FROM {table_name}
+        WHERE trialjobId IN (
+            SELECT trialjobId
+            FROM MetricData
+            WHERE type = 'FINAL'
+            GROUP BY trialjobId
+            ORDER BY MAX(data) DESC
+            LIMIT 5
+        ) 
+        AND type = 'PERIODICAL'
+        ORDER BY trialjobId, sequence;
+    """
+
+
+    # Execute the query
+    cursor.execute(query)
+
+    # Fetch the results
+    results = cursor.fetchall()
+
+    score_dict = {}
+    for row in results:
+        trialjob_id = row[0]
+        sequence = row[1]
+        data = float(row[2].strip('"'))
+
+        # If trialjob_id is not already in the dictionary, add it with an empty list
+        if trialjob_id not in score_dict:
+            score_dict[trialjob_id] = []
+
+        # Append the data to the list, maintaining the order by sequence
+        score_dict[trialjob_id].append(data)
+
+    query2 = f"""
+    SELECT trialjobId, data
+    FROM TrialJobEvent
+    WHERE event = 'WAITING'
+    AND trialjobId IN (SELECT trialjobId
+        FROM MetricData
+        WHERE type = 'FINAL'
+        GROUP BY trialjobId
+        ORDER BY MAX(data) DESC
+        LIMIT 5);
+""" 
+    cursor.execute(query2)
+    results2 = cursor.fetchall()
+    connection.close()
+    params_dict = {}
+    for row in results2:
+        trialjob_id = row[0]
+        parameters = json.loads(row[1])
+        # If trialjob_id is not already in the dictionary, add it with an empty list
+        if trialjob_id not in params_dict:
+        # Append the data to the list, maintaining the order by sequence
+            params_dict[trialjob_id] = (parameters['parameters'])
+
+    merged_dict = merge_dicts(params_dict, score_dict)  
+    
+    if show == True:
+        
+        values = next(iter(merged_dict.values()))
+        header = [['Trials Name'] + list(values[0].keys()) + ['Score']]
+
+        for key, value in merged_dict.items():
+            rows = [key] + list(value[0].values()) + [max(value[1])]
+            header.append(rows)
+
+        print(tabulate(header, headers='firstrow', tablefmt='grid'))    
+
+    return merged_dict
+
+
