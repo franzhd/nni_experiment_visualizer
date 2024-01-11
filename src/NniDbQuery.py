b90e306bad69656a859b7a45abcdc5455ef56727
Francesco Guarino <franzhd@live.it>
2024-01-11 23:43:54 +0100

    function now working with both single and complex nni metrics format

--- src/NniDbQuery.py
+++ src/NniDbQuery.py
@@ -1,11 +1,13 @@
 import sqlite3
 import json
 from tabulate import tabulate
+import matplotlib.pyplot as plt
 
 def merge_dicts(dict1, dict2)-> dict:
     merged_dict = {}
     for key in dict1.keys():
         merged_dict[key] = [dict1[key], dict2[key]]
+    print(merged_dict)
     return merged_dict
 
 
@@ -23,7 +25,7 @@ def nni_query(tiral_sqlite_path, show=True) -> dict:
             FROM MetricData
             WHERE type = 'FINAL'
             GROUP BY trialjobId
-            ORDER BY MAX(data) DESC
+            ORDER BY data DESC
             LIMIT 5
         ) 
         AND type = 'PERIODICAL'
@@ -38,53 +40,134 @@ def nni_query(tiral_sqlite_path, show=True) -> dict:
     results = cursor.fetchall()
     print(results)
     score_dict = {}
+    print(results[0][2])
+    if "{" not in results[0][2]:
+        
+        print("found single Score")
+        query = """
+                UPDATE MetricData
+                SET data = replace(data, '"', '');
+                """
+        
+        cursor.execute(query)
+        connection.commit()
+
+        query = f"""
+                SELECT
+                    trialjobId,
+                    sequence,
+                    CAST(data AS REAL) AS d,
+                    MAX(CAST(data AS REAL)) OVER (PARTITION BY trialjobId) AS max_data
+                FROM MetricData
+                WHERE trialjobId IN (
+                    SELECT trialjobId
+                    FROM MetricData
+                    WHERE type = 'FINAL'
+                    GROUP BY trialjobId
+                    ORDER BY CAST(data AS REAL) DESC
+                    LIMIT 5
+                ) AND type = 'PERIODICAL'
+                ORDER BY  max_data DESC,trialjobId,sequence;
+                """
+            # Execute the query
+        cursor.execute(query)
+        results = cursor.fetchall()
+
+        query = f"""
+                UPDATE MetricData
+                SET data = '"' || data || '"'
+                WHERE data IS NOT NULL;
+                """
+        cursor.execute(query)
+        connection.commit()
+    else:
+        print("found multiple metrics")
+        query = f"""
+                UPDATE MetricData
+                SET data = replace(data, '\\"', '"');
+                """
+        cursor.execute(query)
+        connection.commit()
+        query = f"""
+                UPDATE MetricData
+                SET data = replace(data, '"{{', '{{');
+                """
+        cursor.execute(query)
+        connection.commit()
+        query = f"""
+                UPDATE MetricData
+                SET data = replace(data, '}}"', '}}');
+            """
+        
+        cursor.execute(query)
+        connection.commit()
+        query = f"""
+                SELECT trialjobId,
+                        sequence,
+                        CAST(JSON_EXTRACT(data, '$.default') AS REAL) as metrics,
+                        MAX(CAST(JSON_EXTRACT(data, '$.default') AS REAL)) OVER (PARTITION BY trialjobId) AS max_data
+                FROM MetricData
+                WHERE trialjobId IN (
+                    SELECT trialjobId
+                    FROM MetricData
+                    WHERE type = 'FINAL'
+                    GROUP BY trialjobId
+                    ORDER BY MAX(CAST(JSON_EXTRACT(data, '$.default') AS REAL)) DESC
+                    LIMIT 5
+                ) 
+                AND type = 'PERIODICAL'
+                ORDER BY max_data DESC, trialjobId, sequence;
+            """
+        
+        cursor.execute(query)
+        results = cursor.fetchall()
+
+        query = f"""
+                UPDATE MetricData
+                SET data = replace(data, '"', '\\"');
+                """
+        cursor.execute(query)
+        connection.commit()
+        query = f"""
+                UPDATE MetricData
+                SET data = replace(data, '{{', '"{{');
+                """
+        cursor.execute(query)
+        connection.commit()
+        query = f"""
+                UPDATE MetricData
+                SET data = replace(data, '}}', '}}"');
+            """
+        
+        cursor.execute(query)
+        connection.commit()
+            
+                # Execute the query
+
+
+    # Fetch the results
+    print(results)
     for row in results:
         trialjob_id = row[0]
         sequence = row[1]
-        print(row[2])
-        if isinstance(row[2].strip('"'), float):
-            data = float(row[2].strip('"'))
-        else:
-            query = f"""
-                    SELECT trialjobId, sequence, CAST(JSON_EXTRACT(data, '$.default') AS REAL) as metrics
-                    FROM MetricData
-                    WHERE trialjobId IN (
-                        SELECT trialjobId
-                        FROM MetricData
-                        WHERE type = 'FINAL'
-                        GROUP BY trialjobId
-                        ORDER BY MAX(CAST(JSON_EXTRACT(data, '$.default') AS REAL)) DESC
-                        LIMIT 5
-                    ) 
-                    AND type = 'PERIODICAL'
-                    ORDER BY trialjobId, sequence;
-                """
-            # Execute the query
-            cursor.execute(query)
-            # Fetch the results
-            results = cursor.fetchall()
-            print(results)
-        # If trialjob_id is not already in the dictionary, add it with an empty list
+
+    # If trialjob_id is not already in the dictionary, add it with an empty list
         if trialjob_id not in score_dict:
             score_dict[trialjob_id] = []
 
         # Append the data to the list, maintaining the order by sequence
-        score_dict[trialjob_id].append(data)
-
+        score_dict[trialjob_id].append(row[2])
+    print('score dict', score_dict)
     query2 = f"""
-    SELECT trialjobId, data
-    FROM TrialJobEvent
-    WHERE event = 'WAITING'
-    AND trialjobId IN (SELECT trialjobId
-        FROM MetricData
-        WHERE type = 'FINAL'
-        GROUP BY trialjobId
-        ORDER BY MAX(data) DESC
-        LIMIT 5);
-""" 
-    cursor.execute(query2)
+            SELECT trialjobId, data
+            FROM TrialJobEvent
+            WHERE event = 'WAITING'
+            AND trialjobId IN ({', '.join('?' for _ in score_dict.keys())});
+            """ 
+    cursor.execute(query2, list(score_dict.keys()))
     results2 = cursor.fetchall()
     connection.close()
+
     params_dict = {}
     for row in results2:
         trialjob_id = row[0]
@@ -94,17 +177,22 @@ def nni_query(tiral_sqlite_path, show=True) -> dict:
         # Append the data to the list, maintaining the order by sequence
             params_dict[trialjob_id] = (parameters['parameters'])
 
-    merged_dict = merge_dicts(params_dict, score_dict)  
+    merged_dict = merge_dicts(score_dict, params_dict)  
     
     if show == True:
         
         values = next(iter(merged_dict.values()))
-        header = [['Trials Name'] + list(values[0].keys()) + ['Score']]
-
+        header = [['Trials Name'] + list(values[1].keys()) + ['Score']]
+        plt.figure(figsize=(15, 10))
+        
         for key, value in merged_dict.items():
-            rows = [key] + list(value[0].values()) + [max(value[1])]
+            
+            plt.plot(range(len(value[0])), value[0], label=f'{key}, best:{max(value[0]):.4f}')
+            rows = [key] + list(value[1].values()) + [max(value[0])]
             header.append(rows)
-
+        plt.legend()
+        plt.show()
+        
         print(tabulate(header, headers='firstrow', tablefmt='grid'))    
 
     return merged_dict
